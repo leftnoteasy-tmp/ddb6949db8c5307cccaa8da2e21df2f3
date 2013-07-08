a44f494a042219f12fbcf626b92ffbf0d9a8dfe4
wangda.tan <wheeleast@gmail.com>
2013-07-08 16:54:43 +0800

    add file forgot in last commit :-/

---
+++ hamster-core/src/main/java/com/pivotal/hamster/common/HostExprParser.java
@@ -0,0 +1,10 @@
+package com.pivotal.hamster.common;
+
+public class HostExprParser {
+  /*
+   * TODO, add implementation
+   */
+  public static String parse(String hostExpr) {
+    return null;
+  }
+}
