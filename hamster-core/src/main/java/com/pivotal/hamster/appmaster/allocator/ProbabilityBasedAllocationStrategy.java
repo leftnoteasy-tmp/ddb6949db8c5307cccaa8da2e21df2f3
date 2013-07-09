28ed7095f7b82319c5ba10a77d2039cc7d9373fa
wangda.tan <wheeleast@gmail.com>
2013-07-09 16:42:26 +0800

    finished user policy based allocation strategy impl, ckpt

--- hamster-core/src/main/java/com/pivotal/hamster/appmaster/allocator/ProbabilityBasedAllocationStrategy.java
+++ hamster-core/src/main/java/com/pivotal/hamster/appmaster/allocator/ProbabilityBasedAllocationStrategy.java
@@ -1,6 +1,5 @@
 package com.pivotal.hamster.appmaster.allocator;
 
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -8,174 +7,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.yarn.api.protocolrecords.AllocateResponse;
 import org.apache.hadoop.yarn.api.records.Container;
-import org.apache.hadoop.yarn.api.records.ContainerId;
 import org.apache.hadoop.yarn.api.records.Priority;
-import org.apache.hadoop.yarn.api.records.Resource;
 import org.apache.hadoop.yarn.api.records.ResourceRequest;
-import org.apache.hadoop.yarn.exceptions.YarnRemoteException;
-import org.apache.hadoop.yarn.factories.RecordFactory;
-import org.apache.hadoop.yarn.factory.providers.RecordFactoryProvider;
 import org.apache.hadoop.yarn.util.RackResolver;
 
-import com.pivotal.hamster.appmaster.utils.HamsterAppMasterUtils;
-import com.pivotal.hamster.common.HamsterContainer;
 import com.pivotal.hamster.common.HamsterException;
 
-public class ProbabilityBasedAllocationStrategy implements AllocationStrategy {  
+public class ProbabilityBasedAllocationStrategy extends AllocationStrategyBase {  
   private static final Log LOG = LogFactory.getLog(ProbabilityBasedAllocationStrategy.class);
-
-  static final String ANY = "*";
-  static final int DEFAULT_PRIORITY = 20;
-  
-  // how many MPI proc we need to execute
-  int n;
-  // how many slots we already got to execute MPI proc
-  int m;
-  final RecordFactory recordFactory =
-      RecordFactoryProvider.getRecordFactory(null);
-  ConcurrentLinkedQueue<ContainerId> releaseContainers;
-  Resource resource;
-  boolean verbose;
-  // internal data structure for \vec{a} and \vec{h}
-  Map<String, Integer> hostToId;
-  Map<Integer, List<Container>> hostIdToContainers;
-  int nHosts = 0;
-  YarnContainerAllocator allocator;
-  
-  static class HostIdToProb {
-    String host;
-    double prob;
-    
-    HostIdToProb(String host, double prob) {
-      this.host = host;
-      this.prob = prob;
-    }
-  }
-  
-  static class HostIdToCount {
-    int hostId;
-    int count;
-    
-    HostIdToCount(int hostId, int count) {
-      this.hostId = hostId;
-      this.count = count;
-    }
-  }
   
   public ProbabilityBasedAllocationStrategy(ContainerAllocator allocator, boolean verbose) {
-    this.verbose = verbose;
-    this.allocator = (YarnContainerAllocator)allocator;
-  }
-  
-  @Override
-  public Map<String, List<HamsterContainer>> allocate(int n,
-      ConcurrentLinkedQueue<ContainerId> releaseContainers, Resource resource, Configuration conf)
-      throws HamsterException {
-    // init local variables
-    this.n = n;
-    this.m = 0;
-    this.releaseContainers = releaseContainers;
-    this.resource = resource;
-
-    // init internal data structure
-    hostToId = new HashMap<String, Integer>();
-    hostIdToContainers = new HashMap<Integer, List<Container>>();
-
-    try {
-      // add local node to set
-      String local = HamsterAppMasterUtils.getNormalizedLocalhost();
-      hostToId.put(local, nHosts);
-      hostIdToContainers.put(0, new ArrayList<Container>());
-      nHosts++;
-
-      return internalAllocate();
-    } catch (YarnRemoteException e) {
-      throw new HamsterException(e);
-    } catch (UnknownHostException e) {
-      throw new HamsterException(e);
-    } catch (InterruptedException e) {
-      throw new HamsterException(e);
-    }
-  }
-  
-  Map<String, List<HamsterContainer>> internalAllocate() throws YarnRemoteException, UnknownHostException, InterruptedException {
-    int round = 0;
-    
-    while (n > m) {
-      // get next ask list
-      List<ResourceRequest> askList = getAskList();
-      
-      if (verbose) {
-        printAskList(round, askList);
-      }
-      
-      // assemble request and send to AM
-      int newSlots = makeRemoteRequest(askList);
-      m += newSlots;
-      
-      round++;
-      
-      // sleep for a while before next ask
-      Thread.sleep(200);
-    }
-    
-    LOG.info("STATISTIC: Iterations = " + round );    
-    
-    // release extra containers
-    releaseRedundantContainers();
-    
-    Map<String, List<HamsterContainer>> nodeContainerMap = assembleAllocationResult();
-    LOG.info("STATISTIC: Nodes = " + nodeContainerMap.size() );
-    return nodeContainerMap;
-  }
-  
-  Map<String, List<HamsterContainer>> assembleAllocationResult() {
-    Map<String, List<HamsterContainer>> result = new HashMap<String, List<HamsterContainer>>();
-    int total = 0;
-    
-    // loop allocation and double check if allocation/release are correct
-    for (Entry<String, Integer> entry : hostToId.entrySet()) {
-      if (entry.getValue() != null) {
-        List<Container> containerList = hostIdToContainers.get(entry.getValue());
-        
-        if (null == containerList || containerList.isEmpty()) {
-          continue;
-        }
-        
-        if ((entry.getValue() != 0) && (containerList.size() == 1)) {
-          throw new HamsterException(
-              "this shouldn't happen, all container-list with containers == 1 will be returned, host:["
-                  + entry.getValue()
-                  + "] container count = "
-                  + containerList.size());
-        }
-        
-        if (entry.getValue() == 0) {
-          total += containerList.size();
-        } else {
-          total += containerList.size() - 1;
-        }
-        
-        List<HamsterContainer> hContainers = new ArrayList<HamsterContainer>();
-        for (Container c : containerList) {
-          hContainers.add(new HamsterContainer(c, resource));
-        }
-        result.put(entry.getKey(), hContainers);
-      }
-    }
-    
-    if (total != n) {
-      throw new HamsterException("the final containers in resilt for MPI proc not equals to n");
-    }
-    
-    return result;
+    super(allocator, verbose);
   }
   
   void returnAllContainersInHostId(int hostId) {
@@ -218,6 +64,7 @@ public class ProbabilityBasedAllocationStrategy implements AllocationStrategy {
     hostIdToContainers.put(hostId, newContainers);
   }
   
+  @Override
   void releaseRedundantContainers() {
     int nNeedRelease = m - n;
 
@@ -279,44 +126,7 @@ public class ProbabilityBasedAllocationStrategy implements AllocationStrategy {
     }
   }
   
-  void printAskList(int round, List<ResourceRequest> askList) {
-    
-  }
-  
-  int makeRemoteRequest(List<ResourceRequest> ask) throws YarnRemoteException, UnknownHostException {
-    int nNewSlots = 0; // increased slot number for MPI prob in this allocate
-    
-    AllocateResponse response = allocator.invokeAllocate(ask); 
-    List<Container> allocatedContainer = response.getAMResponse().getAllocatedContainers();
-    for (Container c : allocatedContainer) {
-      String host = HamsterAppMasterUtils.normlizeHostName(c.getNodeId().getHost());
-      
-      // get container list of this host or create a new list
-      List<Container> containerList;
-      if (hostToId.containsKey(host)) {
-        containerList = hostIdToContainers.get(hostToId.get(host));
-        if (containerList == null) {
-          containerList = new ArrayList<Container>();
-          hostIdToContainers.put(hostToId.get(host), containerList);
-        }
-      } else {
-        hostToId.put(host, nHosts);
-        containerList = new ArrayList<Container>();
-        hostIdToContainers.put(nHosts, containerList);
-        nHosts++;
-      }
-      
-      containerList.add(c);
-      if (HamsterAppMasterUtils.isLocalHost(host)) {
-        nNewSlots++;
-      } else if (containerList.size() > 1) {
-        nNewSlots++;
-      }
-    }
-    
-    return nNewSlots;
-  }
-  
+  @Override
   List<ResourceRequest> getAskList() {
     int x; // number of containers need to be asked in this turn
     int y; // number of containers already put into ask list
@@ -412,16 +222,6 @@ public class ProbabilityBasedAllocationStrategy implements AllocationStrategy {
       } else {
         hostAskList.set(0, hostAskList.get(0) + askCount);
       }
-      
-      // add any request 
-//      hostAskList = resourceRequests.get(ANY);
-//      if (null == hostAskList) {
-//        hostAskList = new ArrayList<Integer>();
-//        hostAskList.add(askCount);
-//        resourceRequests.put(ANY, hostAskList);
-//      } else {
-//        hostAskList.set(0, hostAskList.get(0) + askCount);
-//      }
       
       y += askCount;
       if (y >= x) {
--- hamster-core/src/main/java/com/pivotal/hamster/appmaster/allocator/UserPolicyStrategy.java
+++ hamster-core/src/main/java/com/pivotal/hamster/appmaster/allocator/UserPolicyStrategy.java
@@ -1,25 +1,292 @@
 package com.pivotal.hamster.appmaster.allocator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Map.Entry;
+import java.util.Set;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.yarn.api.records.ContainerId;
-import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.yarn.api.records.Container;
+import org.apache.hadoop.yarn.api.records.Priority;
+import org.apache.hadoop.yarn.api.records.ResourceRequest;
+import org.apache.hadoop.yarn.util.RackResolver;
 
-import com.pivotal.hamster.common.HamsterContainer;
+import com.pivotal.hamster.appmaster.utils.HamsterAppMasterUtils;
+import com.pivotal.hamster.common.HamsterConfig;
+import com.pivotal.hamster.common.HamsterException;
 
-public class UserPolicyStrategy implements AllocationStrategy {
+public class UserPolicyStrategy extends AllocationStrategyBase {
+  private static final Log LOG = LogFactory.getLog(UserPolicyStrategy.class);
+
+  // null if no hosts specified
+  Set<String> availableHosts = null;
+  boolean initialized = false;
+  int mproc;
+  int mnode;
+  
   public UserPolicyStrategy(ContainerAllocator allocator, boolean verbose) {
-    
+    super(allocator, verbose);
   }
   
-  @Override
-  public Map<String, List<HamsterContainer>> allocate(int n,
-      ConcurrentLinkedQueue<ContainerId> releaseContainers, Resource resource, Configuration conf) {
-    // TODO Auto-generated method stub
-    return null;
+  void init() {
+    // initialize host list
+    String hostlist = conf.get(HamsterConfig.USER_POLICY_HOST_LIST_KEY);
+    if (null != hostlist) {
+      availableHosts = new HashSet<String>();
+      for (String host : hostlist.split(",")) {
+        if (!host.isEmpty()) {
+          availableHosts.add(HamsterAppMasterUtils.normlizeHostName(host));
+        }
+      }
+    }
+    
+    // initialize mproc, mnode
+    mproc = conf.getInt(HamsterConfig.USER_POLICY_MPROC_KEY, Short.MAX_VALUE);
+    mnode = conf.getInt(HamsterConfig.USER_POLICY_MNODE_KEY, Short.MAX_VALUE);
+    
+    initialized = true;
+  }
+  
+  /**
+   * get upper_bound(x / y)
+   */
+  int getDivisionUpperBound(int x, int y) {
+    int r = x / y;
+    if (x % y == 0) {
+      return r;
+    }
+    
+    return r + 1;
+  }
+  
+  int getMinimumY() {
+    // define left/right bound of y
+    int ly = 1;
+    int ry = Math.min(getDivisionUpperBound(n, availableHosts.size()), mproc);
+    int miny = Integer.MAX_VALUE;
+    
+    while (ly <= ry) {
+      int mid = (ly + ry) >> 1;
+      int sum = 0;
+      
+      // caculate total size based on mid
+      for (String host : availableHosts) {
+        int containersCount = getContainersCountForMpiProc(host);
+        if (containersCount < mid) {
+          sum += mid;
+        } else {
+          sum += containersCount;
+        }
+      }
+      
+      if (sum > n) {
+        ry--;
+        // update miny
+        if (miny > mid) {
+          miny = mid;
+        }
+      } else if (sum < n) {
+        ly++;
+      } else {
+        return mid;
+      }
+    }
+    
+    return miny;
+  }
+  
+  /*
+   * add resource requests for a specified host, we will add rack, ANY if needed
+   */
+  void addResourceRequestForNode(Map<String, Integer> resourceRequests, String host, int askCount) {
+    // add node request
+    addResourceRequests(resourceRequests, host, askCount);
+    
+    // add rack request
+    String rack = RackResolver.resolve(host).getNetworkLocation();
+    addResourceRequests(resourceRequests, rack, askCount);
+    
+    // add any request
+    addResourceRequests(resourceRequests, ANY, askCount);
   }
 
+  @Override
+  List<ResourceRequest> getAskList() {
+    if (!initialized) {
+      init();
+    }
+    
+    if (null == availableHosts || mnode >= 0) {
+      LOG.error("we didn't support not specified availableHosts / specified mnode now");
+      throw new HamsterException("we didn't support not specified availableHosts / specified mnode now");
+    }
+    
+    // we will use a binary search method to search how deep water we put can get total volumn of water
+    // assume we have n barrel, each barrel has already some water in it, e.g. x_1, x_2, ... x_n
+    // x_i is the volumn of water in barrel i, we need m total volumn of water
+    // we need get a mimimum height y, that can make n * y + (sum of volumns higher than y already) >= m
+    // we can use binary search y (1 <= y <= min(upper_bound(m/n), mproc))
+    int miny = getMinimumY();
+    
+    // requests list
+    Map<String, Integer> resourceRequests = new HashMap<String, Integer>();
+    
+    // make allocation list based on miny, we will fill all waters in barrel_i below miny to miny 
+    // (the add count will be (miny - x_i))
+    // and we will add 1 ask to each barrel with x_i >= miny but < mproc to avoid deadlocking
+    for (String host : availableHosts) {
+      int containersCount = getContainersCountForMpiProc(host);
+      if (containersCount < miny) {
+        int askCount = getLackingContainersCount(host, miny);
+        addResourceRequestForNode(resourceRequests, host, askCount);
+      } else {
+        if (containersCount < mproc) {
+          addResourceRequestForNode(resourceRequests, host, 1);
+        }
+      }
+    }
+    
+    // we will assemble List<ResourceRequest>
+    List<ResourceRequest> ret = new ArrayList<ResourceRequest>();
+    for (Entry<String, Integer> entry : resourceRequests.entrySet()) {
+      int slotNum = entry.getValue();
+      ResourceRequest req = recordFactory
+          .newRecordInstance(ResourceRequest.class);
+      Priority pri = recordFactory.newRecordInstance(Priority.class);
+      pri.setPriority(DEFAULT_PRIORITY);
+      req.setCapability(resource);
+      req.setHostName(entry.getKey());
+      req.setNumContainers(slotNum);
+      req.setPriority(pri);
+      ret.add(req);
+    }
+    
+    return null;
+  }
+  
+  private void putContainersToMpiProcSizeMap(int mpiProc,
+      List<Container> containers, Map<Integer, List<List<Container>>> map) {
+    if (map.get(mpiProc) == null) {
+      map.put(mpiProc, new ArrayList<List<Container>>());
+    }
+    map.get(mpiProc).add(containers);
+  }
+
+  @Override
+  void releaseRedundantContainers() {
+    int nNeedRelease = m - n;
+    
+    // there're two steps to release redundant containers,
+    // first, we will scan all allocated containers, if containers count in one host are <= 1 (0 for hnp node)
+    // we will discard such containers
+    // then we will deduct containers from more to less
+    // get host id to counts, we will release host will less containers first
+    HostIdToCount[] hostIdToCounts = new HostIdToCount[hostIdToContainers
+        .size() - 1];
+    int idx = 0;
+    for (Entry<Integer, List<Container>> entry : hostIdToContainers.entrySet()) {
+      if (entry.getKey() != 0) {
+        hostIdToCounts[idx] = new HostIdToCount(entry.getKey(), entry
+            .getValue().size());
+        idx++;
+      }
+    }
+    
+    // loop hosts and release container cannot run MPI proc
+    for (String host : availableHosts) {
+      List<Container> containers = getContainersByHost(host);
+      int mpiProc = getContainersCountForMpiProc(host);
+      if ((containers != null) && (containers.size() > 0) && (mpiProc <= 0)) {
+        for (Container container : containers) {
+          releaseContainers.add(container.getId());
+        }
+        containers.clear();
+      }
+    }
+    
+    // we will put all containers to a map<int, List<List<containers>>
+    // it's <mpi_proc_size, <container-list-host_a, container_list_host_b, ...>>
+    Map<Integer, List<List<Container>>> nodeSizeToContainersLists = new HashMap<Integer, List<List<Container>>>();
+    for (String host : availableHosts) {
+      List<Container> containers = getContainersByHost(host);
+      int mpiProc = getContainersCountForMpiProc(host);
+      
+      // skip such node
+      if (mpiProc == 0) {
+        // sanity check
+        if (containers != null && !containers.isEmpty()){
+          LOG.error("this shouldn't happen, if #mpi-proc > 0 in one node, the container list should be empty");
+          throw new HamsterException("this shouldn't happen, if #mpi-proc > 0 in one node, the container list should be empty");
+        }
+        continue;
+      }
+      
+      // sanity check
+      if (mpiProc > 0 && (containers == null || containers.isEmpty())) {
+        LOG.error("this shouldn't happen, all node with mpi_proc > 0 should have enough containers");
+        throw new HamsterException("this shouldn't happen, all node with mpi_proc > 0 should have enough containers");
+      }
+      
+      putContainersToMpiProcSizeMap(mpiProc, containers, nodeSizeToContainersLists);
+    }
+    
+    // we will return containers from reversed order
+    int maxMpiProcCount = -1;
+    for (Integer key : nodeSizeToContainersLists.keySet()) {
+      if (key > maxMpiProcCount) {
+        maxMpiProcCount = key;
+      }
+    }
+    
+    int key = maxMpiProcCount;
+    while (key > 0 && nNeedRelease > 0) {
+      List<List<Container>> containersLists = nodeSizeToContainersLists.get(key);
+      if (null == containersLists) {
+        key--;
+        continue;
+      }
+      for (int i = containersLists.size() - 1; i >= 0; i--) {
+        if (nNeedRelease <= 0) {
+          break;
+        }
+        List<Container> containers = containersLists.get(i);
+        // if we have only one container for mpi proc on this node, and this
+        // container will be return, we will return all containers in this node
+        if (key == 1) {
+          for (Container container : containers) {
+            releaseContainers.add(container.getId());
+          }
+        } else {
+          releaseContainers.add(containers.get(containers.size() - 1).getId());
+          containers.remove(containers.size() - 1);
+        }
+        // we removed one container in this host, so it can launch key - 1
+        // mpi_proc, we will add it to level below
+        containersLists.remove(i);
+        if (key > 1) {
+          putContainersToMpiProcSizeMap(key - 1, containers, nodeSizeToContainersLists);
+        }
+        // update nNeedRelease
+        nNeedRelease--;
+      }
+    }
+  }
+
+  @Override
+  // check host is available during allocation
+  boolean checkIsHostAvailable(String host) {
+    if (availableHosts != null && !availableHosts.contains(host)) {
+      LOG.info("received host not in user specified hostlist, host:" + host);
+      return false;
+    }
+    if (getContainersCountForMpiProc(host) >= mproc) {
+      LOG.info("this host already have enough containers, discard this one");
+      return false;
+    }
+    return true;
+  }
 }
