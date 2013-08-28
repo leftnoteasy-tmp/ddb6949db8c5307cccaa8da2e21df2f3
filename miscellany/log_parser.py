3928c7739a11f51d5045fee086ba994c29f59767
wangda.tan <wheeleast@gmail.com>
2013-08-28 15:28:52 +0800

    add fix for log parser script

--- miscellany/log_parser.py
+++ miscellany/log_parser.py
@@ -1,32 +1,25 @@
 import sys
-import argparse
 
 if __name__ == "__main__":
     FILTER_STR = 'STATISTIC: '
 
-    # add argument
-    parser = argparse.ArgumentParser(description = "Parse YARN log for Hamster")
-    parser.add_argument('-i')
-    parser.add_argument('-d')
-    parser.add_argument('-o')
-    args = parser.parse_args(sys.argv[1:])
-
-    if args.i == None or args.d == None or args.o == None:
-        raise "must specify i/d/o"
+    if len(sys.argv) != 4:
+        raise IOError("using python log_parser.py outfile infile desc")
 
     # open file
-    ifs = open(args.i, 'r')
+    ifs = open(sys.argv[2], 'r')
     try:
-        ofs = open(args.o, 'a')
+        ofs = open(sys.argv[1], 'a')
     except IOError:
-        ofs = open(args.o, 'w')
+        ofs = open(sys.argv[1], 'w')
     ofs.write('\n')
     ofs.write('==========================================\n')
-    ofs.write(args.d + '\n')
+    ofs.write(sys.argv[3] + '\n')
     ofs.write('==========================================\n')
     line = ifs.readline()
     while len(line) > 0:
         if line.find(FILTER_STR) >= 0:
             out = line[line.find(FILTER_STR) + len(FILTER_STR): len(line)]
+            print out[:len(out)-1]
             ofs.write(out)
         line = ifs.readline()
